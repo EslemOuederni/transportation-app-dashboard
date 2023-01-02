11b83fe1e42920179df9cd0a1e7dd70637da9512
essss <sousououederni@gmail.com>
2023-01-02 22:53:44 +0100

    auth0

--- server.js
+++ server.js
@@ -1,6 +1,18 @@
 const express = require("express");
-
+const { auth } = require("express-openid-connect");
 const app = express();
+require("dotenv").config();
+
+const config = {
+  authRequired: false,
+  auth0Logout: true,
+  secret: process.env.SECRET,
+  baseURL: process.env.BASE_URL,
+  clientID: process.env.CLIENT_ID,
+  issuerBaseURL: process.env.ISSUER,
+};
+
+app.use(auth(config));
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
