dba6b0622359df687e1131ce7ab7efdbb4dd3b65
essss <sousououederni@gmail.com>
2023-01-09 18:34:29 +0100

    Auth Context

---
+++ client/src/Context/AuthContext.tsx
@@ -0,0 +1,63 @@
+import axios from "axios";
+import React, { createContext, useState } from "react";
+
+export interface IUser {
+  _v: number;
+  _id: string;
+  firstName: string;
+  lastName: string;
+  email: string;
+  password: string;
+}
+export interface IUserType {
+  _v: number;
+  _id: string;
+  firstName: string;
+  lastName: string;
+  email: string;
+  password: string;
+  fetchCurrentUser: () => void;
+}
+const initialContext: IUserType = {
+  _v: 0,
+  _id: "",
+  firstName: "",
+  lastName: "",
+  email: "",
+  password: "",
+  fetchCurrentUser: () => {},
+};
+
+export const CurrentUserContext = createContext<IUserType>(initialContext);
+
+type AuthProviderProps = {
+  children: React.ReactNode;
+};
+
+export const CurrentUserProvider = ({ children }: AuthProviderProps) => {
+  const [currentUser, setCurrentUser] = useState<IUserType>(initialContext);
+
+  const fetchCurrentUser = async () => {
+    try {
+      axios
+        .get("http://localhost:3000/api/admin/getOneAdmin", {
+          headers: {
+            "Access-Control-Allow-Origin": "*",
+            Authorization: `Bearer ${localStorage.getItem("token")}`,
+          },
+        })
+        .then((res) => {
+          console.log(res.data);
+          setCurrentUser(res.data);
+        });
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return (
+    <CurrentUserContext.Provider value={{ ...currentUser, fetchCurrentUser }}>
+      {children}
+    </CurrentUserContext.Provider>
+  );
+};
