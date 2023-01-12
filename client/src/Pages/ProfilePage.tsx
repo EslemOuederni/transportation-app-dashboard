b9a0f1216fb29139856cff4b369bc87d57355c89
essss <sousououederni@gmail.com>
2023-01-12 17:22:18 +0100

    Pages

---
+++ client/src/Pages/ProfilePage.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ProfilePage = () => {
+  return <> Here's my profile</>;
+};
+
+export default ProfilePage;
---
+++ client/src/Pages/TransportPage.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const TransportPage = () => {
+  return <div>TransportPage</div>;
+};
+
+export default TransportPage;
