6d504382c7592724a1e058206cde93bd2799faf9
essss <sousououederni@gmail.com>
2023-01-18 18:31:18 +0100

    protected

--- client/src/Components/Protected.jsx
+++ client/src/Components/Protected.jsx
@@ -1,9 +1,14 @@
 import React from "react";
-// import { useAuth } from "../Hooks/useAuth";
+import { useAuth } from "../Hooks/useAuth";
 import { Navigate } from "react-router-dom";
 
 const Protected = ({ children }) => {
-  return children;
+  const user = JSON.parse(localStorage.getItem("user"));
+  console.log(user);
+  if (!user) {
+    return <Navigate to="/auth/login" />;
+  }
+  return <>{children}</>;
 };
 
 export default Protected;
