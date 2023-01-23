88c4373c63ff8f34314152efa593e3f1a31a39d0
essss <sousououederni@gmail.com>
2023-01-23 14:36:06 +0100

    index

--- server/routes/index.routes.js
+++ server/routes/index.routes.js
@@ -3,7 +3,11 @@ const express = require("express");
 const router = express.Router();
 
 const adminRouter = require("./Admin.routes");
+const transportRouter = require("./Transport.routes");
+const ticketRouter = require("./Ticket.routes");
 
 router.use("/api/admin", adminRouter);
+router.use("/api/transport", transportRouter);
+router.use("/api/ticket", ticketRouter);
 
 module.exports = router;
