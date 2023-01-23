2676563a315235548e409cf13339716acbb876d9
essss <sousououederni@gmail.com>
2023-01-23 20:36:09 +0100

    Add Vehicule

---
+++ client/src/Pages/AddVehicule.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import AddForm from "../Components/AddForm";
+
+const AddVehicule = () => {
+  return (
+    <div>
+      <AddForm />
+    </div>
+  );
+};
+
+export default AddVehicule;
