eabbd833813b7f3bacdf722ceab33d2e73b073c9
essss <sousououederni@gmail.com>
2023-01-25 19:31:47 +0100

    Trip form

---
+++ client/src/Pages/AddTrip.jsx
@@ -0,0 +1,116 @@
+import React from "react";
+import { useState } from "react";
+import Input from "../Components/Input";
+import { useEffect } from "react";
+import axios from "axios";
+const AddTrip = () => {
+  const transportationMean = ["Bus", "Train", "Plane", "Car"];
+  const [transport, setTransport] = useState(transportationMean[0]);
+  const [departureDate, setDepartureDate] = useState(new Date());
+  const [arrivalDate, setArrivalDate] = useState(new Date());
+  const [departureCity, setDepartureCity] = useState(null);
+  const [arrivalCity, setArrivalCity] = useState(null);
+  const [status, setStatus] = useState("");
+  const [numberOfTickets, setNbrOfTickets] = useState(0);
+  const [cities, setCities] = useState([]);
+
+  const getCities = () => {
+    axios.get("http://localhost:3000/api/city").then((res) => {
+      setCities([...res.data]);
+    });
+  };
+  useEffect(() => {
+    getCities();
+  }, []);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const trip = {
+      transport,
+      departureDate,
+      arrivalDate,
+      departureCity,
+      arrivalCity,
+      status,
+      numberOfTickets,
+    };
+    console.log(trip);
+    let response = axios
+      .post("http://localhost:3000/api/trip/", trip)
+      .then((res) => {
+        console.log(res.data);
+      })
+      .catch((error) => {
+        console.log(error.response.data);
+      });
+  };
+  return (
+    <div className=" ml-[300px] flex flex-col md:ml-[400px] lg:mx-[450px] lg:p-3">
+      <h1 className=" font-semibold text-2xl mb-3">Add Form</h1>
+      <div className="flex flex-col border-slate-700 border-2 px-10 py-6">
+        <label className="py-2">Mean of Transportation : </label>
+        <select
+          onChange={(e) => setTransport(e.target.value)}
+          className="px-4 py-2 rounded-md uppercase font-semibold"
+        >
+          {transportationMean.map((item, index) => (
+            <option className="" key={index}>
+              {item}
+            </option>
+          ))}
+        </select>
+        <Input
+          name="Departure Date"
+          type="date"
+          value={departureDate}
+          onChange={(e) => setDepartureDate(e.target.value)}
+          min={new Date().toISOString().split("T")[0]}
+        />
+        <Input
+          name="Arrival Date"
+          type="date"
+          value={arrivalDate}
+          onChange={(e) => setArrivalDate(e.target.value)}
+          min={new Date().toISOString().split("T")[0]}
+        />
+        <label className="py-2">Departure City : </label>
+        <select
+          onChange={(e) => setDepartureCity(e.target.value)}
+          className="px-4 py-2 rounded-md uppercase font-semibold"
+        >
+          {cities.map((item, index) => (
+            <option className="" key={index}>
+              {item.name}
+            </option>
+          ))}
+        </select>
+        <label className="py-2">Arrival City : </label>
+        <select
+          onChange={(e) => setArrivalCity(e.target.value)}
+          className="px-4 py-2 rounded-md uppercase font-semibold"
+        >
+          {cities.map((item, index) => (
+            <option className="" key={index}>
+              {item.name}
+            </option>
+          ))}
+        </select>
+        <Input
+          name="number of tickets"
+          type="number"
+          value={numberOfTickets}
+          onChange={(e) => setNbrOfTickets(e.target.value)}
+          min="1"
+        />
+        <button
+          className=" mt-4 bg-blue-700 rounded-md px-4 py-2 text-white font-semibold mb-4"
+          onClick={handleSubmit}
+        >
+          Add
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default AddTrip;
