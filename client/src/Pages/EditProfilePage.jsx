b5e998cf23bfc967a7ab315f93c2256c88b2cec4
essss <sousououederni@gmail.com>
2023-02-08 18:43:11 +0100

    modal

---
+++ client/src/Pages/EditProfilePage.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const EditProfilePage = () => {
+  return (
+    <div className=" ml-[300px]">
+      <h1>Edit Profile Page</h1>
+    </div>
+  );
+};
+
+export default EditProfilePage;
