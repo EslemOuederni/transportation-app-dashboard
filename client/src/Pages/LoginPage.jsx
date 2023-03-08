1b2ddb1ec2bf1a88c9426dafe454190522ec9d3d
essss <sousououederni@gmail.com>
2023-03-08 17:32:05 +0100

    centering

--- client/src/Pages/LoginPage.jsx
+++ client/src/Pages/LoginPage.jsx
@@ -2,6 +2,7 @@ import axios from "axios";
 import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { useSignin } from "../Hooks/useSignIn";
+import logo from "../assets/logo.svg";
 
 const LoginPage = () => {
   const [email, setEmail] = useState("");
@@ -18,55 +19,52 @@ const LoginPage = () => {
   };
 
   return (
-    <>
-      <div className=" w-full h-screen ">
-        <div className="max-w-[450px] h-[520px] bg-slate-900 text-white mt-9 rounded-lg ">
-          <div className="max-w-[320px] mx-auto py-16">
-            <h1 className="text-3xl font-bold">Sign In</h1>
-            <form
-              onSubmit={handleSubmit}
-              className="w-full flex flex-col py-4 mb-4"
-            >
-              {error && (
-                <div className="bg-red-600 p-3 rounded mb-4">
-                  <p className="text-sm">{error}</p>
-                </div>
-              )}
-              <input
-                onChange={(e) => setEmail(e.target.value)}
-                className="p-3 my-2 bg-gray-700 rounded"
-                type="email"
-                placeholder="Email"
-                autoComplete="email"
-              />
-              <input
-                onChange={(e) => setPassword(e.target.value)}
-                className="p-3 my-2 bg-gray-700 rounded"
-                type="password"
-                placeholder="password"
-              />
-              <button
-                className="bg-red-600 py-3 my-6 rounded font-bold"
-                disabled={loading}
-              >
-                Sign In
-              </button>
-              <div className="flex justify-between items-center text-sm text-gray-600">
-                <p>
-                  <input className="mr-2" type="checkbox" />
-                  Remember me
-                </p>
-                <Link to="/auth/resetPwd">Forgot Password ?</Link>
-              </div>
-              <p className="py-8">
-                <span className="text-gray-600">New Here ?</span>{" "}
-                <Link to="/auth/register">Sign Up</Link>
-              </p>
-            </form>
+    <div className="max-w-[450px] h-[500px] bg-slate-900 text-white rounded-lg mx-auto ">
+      <div className="max-w-[320px] mx-auto py-16">
+        <h1 className="text-center text-3xl mb-4">Welcome Back</h1>
+        {/* <h1 className="text-3xl font-bold">Sign In</h1> */}
+        <form
+          onSubmit={handleSubmit}
+          className="w-full flex flex-col py-4 mb-4"
+        >
+          {error && (
+            <div className="bg-red-600 p-3 rounded mb-4">
+              <p className="text-sm">{error}</p>
+            </div>
+          )}
+          <input
+            onChange={(e) => setEmail(e.target.value)}
+            className="p-3 my-2 bg-gray-700 rounded outline-none border-transparent focus:outline-white"
+            type="email"
+            placeholder="Email"
+            autoComplete="email"
+          />
+          <input
+            onChange={(e) => setPassword(e.target.value)}
+            className="p-3 my-2 bg-gray-700 rounded outline-none border-transparent focus:outline-white"
+            type="password"
+            placeholder="password"
+          />
+          <button
+            className="bg-red-600 py-3 my-6 rounded font-bold"
+            disabled={loading}
+          >
+            Sign In
+          </button>
+          <div className="flex justify-between items-center text-sm text-gray-600">
+            <Link to="/auth/resetPwd" className=" text-red-500 mx-auto">
+              Forgot Your Password ?
+            </Link>
           </div>
-        </div>
+          <p className="py-8 mx-auto">
+            <span className="text-gray-600">New Here ?</span>{" "}
+            <Link to="/auth/register" className=" text-red-500">
+              Sign Up
+            </Link>
+          </p>
+        </form>
       </div>
-    </>
+    </div>
   );
 };
 
--- client/src/Pages/RegisterPage.jsx
+++ client/src/Pages/RegisterPage.jsx
@@ -19,62 +19,53 @@ const RegisterPage = () => {
   };
 
   return (
-    <>
-      <div className=" w-full h-screen">
-        <div className="max-w-[450px] h-[550px] bg-slate-900 text-white mt-9 rounded-lg">
-          <div className="max-w-[320px] mx-auto py-16">
-            <h1 className="text-3xl font-bold">Register</h1>
-            <form
-              onSubmit={handleSubmit}
-              className="w-full flex flex-col py-4 mb-4"
-            >
-              {error && <p className="text-red-600">{error}</p>}
-              <input
-                onChange={(e) => setEmail(e.target.value)}
-                className="p-3 my-2 bg-gray-700 rounded"
-                placeholder="Email"
-                autoComplete="email"
-              />
-              <input
-                onChange={(e) => setPassword(e.target.value)}
-                className="p-3 my-2 bg-gray-700 rounded"
-                type="password"
-                placeholder="password"
-              />
-              <input
-                onChange={(e) => setFirstName(e.target.value)}
-                className="p-3 my-2 bg-gray-700 rounded"
-                type="text"
-                placeholder="First Name"
-              />
-              <input
-                onChange={(e) => setLastName(e.target.value)}
-                className="p-3 my-2 bg-gray-700 rounded"
-                type="text"
-                placeholder="Last Name"
-              />
-              <button
-                className="bg-red-600 py-3 my-6 rounded font-bold"
-                disabled={loading}
-              >
-                Sign Up
-              </button>
-              <div className="flex justify-between items-center text-sm text-gray-600">
-                <p>
-                  <input className="mr-2" type="checkbox" />
-                  Remember me
-                </p>
-                <p>Need Help?</p>
-              </div>
-              <p className="text-center">
-                <span className="text-gray-600">Already subscribed?</span>{" "}
-                <Link to="/auth/login">Sign In</Link>
-              </p>
-            </form>
-          </div>
-        </div>
+    <div className="max-w-[450px] h-[550px] bg-slate-900 text-white rounded-lg mx-auto">
+      <div className="max-w-[320px] mx-auto py-16">
+        <h1 className="text-3xl font-bold">Register</h1>
+        <form
+          onSubmit={handleSubmit}
+          className="w-full flex flex-col py-4 mb-4"
+        >
+          {error && <p className="text-red-600">{error}</p>}
+          <input
+            onChange={(e) => setEmail(e.target.value)}
+            className="p-3 my-2 bg-gray-700 rounded"
+            placeholder="Email"
+            autoComplete="email"
+          />
+          <input
+            onChange={(e) => setPassword(e.target.value)}
+            className="p-3 my-2 bg-gray-700 rounded"
+            type="password"
+            placeholder="password"
+          />
+          <input
+            onChange={(e) => setFirstName(e.target.value)}
+            className="p-3 my-2 bg-gray-700 rounded"
+            type="text"
+            placeholder="First Name"
+          />
+          <input
+            onChange={(e) => setLastName(e.target.value)}
+            className="p-3 my-2 bg-gray-700 rounded"
+            type="text"
+            placeholder="Last Name"
+          />
+          <button
+            className="bg-red-600 py-3 my-6 rounded font-bold"
+            disabled={loading}
+          >
+            Sign Up
+          </button>
+          <p className="text-center">
+            <span className="text-gray-600">Already subscribed?</span>{" "}
+            <Link to="/auth/login" className=" text-red-500">
+              Sign In
+            </Link>
+          </p>
+        </form>
       </div>
-    </>
+    </div>
   );
 };
 
