9cfcbd4232d2acf497b4d1df6b0233dd0b7961ea
essss <sousououederni@gmail.com>
2023-03-08 17:21:10 +0100

    add default colors

--- client/tailwind.config.cjs
+++ client/tailwind.config.cjs
@@ -2,7 +2,13 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#110D15",
+        secondary: "#D5E87E",
+        tertiary: "#d89cff",
+      },
+    },
   },
   plugins: [],
 };
