38274d3cfcd8758ca7ba329431a306abe1aa5035
essss <sousououederni@gmail.com>
2023-03-10 20:50:32 +0100

    update form

--- client/src/Components/UpdateForm.jsx
+++ client/src/Components/UpdateForm.jsx
@@ -1,36 +1,31 @@
 import React from "react";
 
-const UpdateForm = ({
-  capacity,
-  description,
-  setCapacity,
-  setDescription,
-  updateTransport,
-}) => {
+const UpdateForm = ({ capacity, description, setCapacity, setDescription }) => {
   return (
-    <div className=" flex flex-col lg:p-3">
-      <h1 className=" font-semibold text-2xl mb-3">Add Form</h1>
-      <div className="flex flex-col border-slate-700 border-2 px-10 py-6">
-        <form className="flex flex-col">
-          <label>Edit</label>
-
-          <label>capacity</label>
-          <input
-            name="capacity"
-            type="number"
-            value={capacity}
-            onChange={(e) => setCapacity(e.target.value)}
-          />
-          <label>description</label>
-          <input
-            name="description"
-            type="text"
-            value={description}
-            onChange={(e) => setDescription(e.target.value)}
-          />
-        </form>
+    <form className="flex flex-col justify-center">
+      <div className="mt-4 flex flex-col">
+        <label className="font-semibold mb-2">Capacity</label>
+        <input
+          name="capacity"
+          type="number"
+          value={capacity}
+          onChange={(e) => setCapacity(e.target.value)}
+          className=" outline-none border border-slate-300 rounded-md p-1"
+        />
       </div>
-    </div>
+      <div className="mt-4 flex flex-col">
+        <label className=" font-semibold mb-2">Description</label>
+        <textarea
+          name="description"
+          type="textarea"
+          value={description}
+          onChange={(e) => setDescription(e.target.value)}
+          className="h-auto outline-none border border-slate-300 rounded-md p-1"
+          maxlength="100"
+          minlength="10"
+        />
+      </div>
+    </form>
   );
 };
 
