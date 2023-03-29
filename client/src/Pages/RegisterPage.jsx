29303202c10588e8a825d7a2bc2cbff575f1d46a
essss <sousououederni@gmail.com>
2023-03-29 16:15:06 +0000

    import issue

--- client/src/Pages/RegisterPage.jsx
+++ client/src/Pages/RegisterPage.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
-import { UseSignUp } from "../Hooks/useSignup";
+import { UseSignUp } from "../Hooks/useSignUp";
 
 const RegisterPage = () => {
   const [email, setEmail] = useState("");
