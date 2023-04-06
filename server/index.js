469aaa2f4aa53ec2439eb1a379adf8fcf94529af
essss <sousououederni@gmail.com>
2023-04-06 15:43:41 +0100

    origin

--- server/index.js
+++ server/index.js
@@ -12,6 +12,16 @@ app.use(
     origin: "https://transportation-app-dashboard-client.vercel.app",
   })
 );
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, PATCH"
+  );
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
+});
+
 //routes
 app.use(router);
 
