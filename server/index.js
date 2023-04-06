83e30a9ed52b88d4319700d8780cb6d2c5a9bf76
essss <sousououederni@gmail.com>
2023-04-06 15:39:28 +0100

    fuck cors

--- server/index.js
+++ server/index.js
@@ -9,7 +9,7 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: "https://transportation-app-dashboard-server.vercel.app",
+    origin: "https://transportation-app-dashboard-client.vercel.app",
   })
 );
 //routes
