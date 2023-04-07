4b25ff63567c5a717ed9035229f4f0b66df83c6f
essss <sousououederni@gmail.com>
2023-04-07 14:11:36 +0100

    cache the barchart

--- client/src/Components/BarChartComponent.jsx
+++ client/src/Components/BarChartComponent.jsx
@@ -1,9 +1,8 @@
 import React, { useEffect, useState } from "react";
 import Chart from "chart.js/auto";
-import { Bar, Pie } from "react-chartjs-2";
+import { Bar } from "react-chartjs-2";
 import axios from "axios";
 import { baseURL } from "../api";
-
 const ChartComponent = () => {
   const [chartData, setChartData] = useState([]);
   const [sortedData, setSortedData] = useState([]);
@@ -29,16 +28,13 @@ const ChartComponent = () => {
   };
 
   const getTicketData = async () => {
-    const res = await axios.get(`${baseURL}/ticket/count/month`);
-    setChartData(res.data);
-    return res.data;
-  };
-  useEffect(() => {
-    getTicketData();
-  }, []);
-  useEffect(() => {
-    setSortedData(
-      chartData
+    const cacheKey = "ticketData";
+    const cachedData = localStorage.getItem(cacheKey);
+
+    if (cachedData) {
+      console.log("chart (from cache):", JSON.parse(cachedData));
+      setChartData(JSON.parse(cachedData));
+      const sortedData = JSON.parse(cachedData)
         .map((item) => {
           const month = matchMonth(item._id);
           return {
@@ -46,12 +42,36 @@ const ChartComponent = () => {
             count: item.count,
           };
         })
-        .sort((a, b) => a.month - b.month)
-    );
+        .sort((a, b) => a.month - b.month);
+      setSortedData(sortedData);
+      setMonth(sortedData.map((item) => item.month));
+      setCount(sortedData.map((item) => item.count));
+      return JSON.parse(cachedData);
+    }
 
+    const res = await axios.get(`${baseURL}/ticket/count/month`);
+    console.log("chart:", res.data);
+    setChartData(res.data);
+
+    localStorage.setItem(cacheKey, JSON.stringify(res.data));
+
+    const sortedData = res.data
+      .map((item) => {
+        const month = matchMonth(item._id);
+        return {
+          month: month,
+          count: item.count,
+        };
+      })
+      .sort((a, b) => a.month - b.month);
+    setSortedData(sortedData);
     setMonth(sortedData.map((item) => item.month));
     setCount(sortedData.map((item) => item.count));
-  }, [chartData]);
+    return res.data;
+  };
+  useEffect(() => {
+    getTicketData();
+  }, []);
 
   const data = {
     labels: month,
